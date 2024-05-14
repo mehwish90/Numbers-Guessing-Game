f0d4094e70700efcaf702db4a833e253be94e3d9
mehwish90 <mehwishbano141@gmail.com>
2024-05-15 01:51:57 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//1) computer generate randam number
+//2) user input for guessing number
+//3) compare user input with computer generate number and show result
+
+const randamnumber=Math.floor(Math.random()*7+3);
+const answer=await inquirer.prompt([
+    {
+        name: "usernumber",
+        type:"number",
+        message:"please enter your guessing number betwwen 1 - 10:",
+    }
+]); 
+if (answer.usernumber===randamnumber){
+    console.log("CONGRATULATION! you guess rigth number")
+}else(console.log("OOH! you guess wrong number"));
